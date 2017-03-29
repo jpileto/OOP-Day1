82b2fc9bee30e672bb67aa019963ef365c668976
jpileto <jp.ileto@icloud.com>
2017-03-29 15:42:50 -0400

    doing the deliver method in the paperboy exercise

--- paperboy.rb
+++ paperboy.rb
@@ -1,9 +1,34 @@
 class Paperboy
+
+  attr_reader :name, :earnings
+  attr_accessor :experience
+
   def initialize(name, experience, earnings)
     @name = name
     @experience = experience
     @earnings = earnings
+  end
+
+  def quota
+    quota = 50 + (@experience * 0.5)
+  end
+
+  def deliver(start_address, end_address)
+    @experience = end_address - start_address
+    if quota <= 50
+      @earnings = @experience * 0.25
+      @experience += deliver
+    else
+      @earnings = quota * 0.25
+
+    end
 
   end
 
+
 end
+
+jp = Paperboy.new("JP", 0, 0)
+# puts jp.deliver(1, 51)
+# puts jp.experience
+puts jp.quota
